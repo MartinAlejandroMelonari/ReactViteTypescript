775e96ef7f4d850698899a7b8154424fcce5e00d
MartinAlejandroMelonari <144966823+MartinAlejandroMelonari@users.noreply.github.com>
2023-11-07 02:59:09 -0300

    Modal y ProductModal
    Modal y ProductModal

--- src/components/ProductTable/ProductTable.tsx
+++ src/components/ProductTable/ProductTable.tsx
@@ -3,6 +3,31 @@ import { Product } from "../../types/Product";
 import { ProductService } from "../../services/ProductoService";
 import Loader from "../Loader/Loader";
 import { Table } from "react-bootstrap";
+import { ModalType } from "../../types/ModalType";
+
+const initializableNewProduct = (): Product => {
+    return{
+        id:0,
+        title: "",
+        price: 0,
+        description: "",
+        category: "",
+        image: ""
+    };
+};
+
+const [product, setProduct] = useState<Product>(initializableNewProduct);
+
+const [showModal, setShowModal] = useState(false);
+const [modalType, setModalType] = useState<ModalType>(ModalType.NONE);
+const [title, setTitle] = useState("");
+
+const handleClick = (newTitle: string, prod: Product, modal: ModalType) => {
+    setTitle(newTitle);
+    setModalType(modal);
+    setProduct(prod);
+    setShowModal(true);
+}
 
 const ProductTable = () => {
     const [products, setProducts] = useState <Product[]>([]);
@@ -23,6 +48,7 @@ const ProductTable = () => {
 
     return(
         <>
+        <Button onClick={() => handleClick("Nuevo producto", initializableNewProduct(), ModalType.CREATE)}>Nuevo producto</Button>
         {isLoading?<Loader/>:(
             <Table hover>
                 <thead>
